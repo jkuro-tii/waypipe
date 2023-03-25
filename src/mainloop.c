jkuro-tii/waypipe

commit a0f6bfa191f55b99e4ff68dd0063aa0c0e12dcbd

Insert block ack messages early in transfer list
This should reduce peak memory usage on connections with large
bidirectional buffer transfers, by allowing the Waypipe instances
to clean up old messages earlier.

This change is also preparation for allowing Waypipe to limit
the amount of data queued for transfer; sending acknowledgements
out of order avoids deadlocks.

src/mainloop.c
@@ -272,7 +272,10 @@ static int interpret_chanmsg(struct chan_msg_state *cmsg,
 		return 0;
 	} else if (type == WMSG_ACK_NBLOCKS) {
 		struct wmsg_ack *ackm = (struct wmsg_ack *)packet;
-		cxs->last_confirmed_msgno = ackm->messages_received;
+
+		if (ackm->messages_received >= cxs->last_received_msgno) {
+			cxs->last_confirmed_msgno = ackm->messages_received;
+		}
 		return 0;
 	} else {
 		cxs->last_received_msgno++;
@@ -686,6 +689,65 @@ static int advance_waymsg_chanwrite(struct way_msg_state *wmsg,
 
 	// First, clear out any transfers that are no longer needed
 	clear_old_transfers(&wmsg->transfers, cxs->last_confirmed_msgno);
+
+	/* Acknowledge the other side's transfers as soon as possible */
+	if (cxs->last_acked_msgno != cxs->last_received_msgno) {
+		/* TODO: consider statically allocating two slots for ack
+		 * messages, and updating the next slot in use if applicable */
+		struct wmsg_ack *ackm = calloc(1, sizeof(struct wmsg_ack));
+		if (!ackm) {
+			wp_error("Failed to allocate transfer acknowledgement message");
+			goto ackmsg_fail;
+		}
+		ackm->size_and_type = transfer_header(
+				sizeof(struct wmsg_ack), WMSG_ACK_NBLOCKS);
+
+		if (transfer_ensure_size(&wmsg->transfers,
+				    wmsg->transfers.end + 1) == -1) {
+			wp_error("Failed to allocate space for ack message transfer");
+			free(ackm);
+			goto ackmsg_fail;
+		}
+
+		/* Reorder the messages to introduce the acknowledgement
+		 * as early as possible in the stream */
+		int next_slot = (wmsg->transfers.partial_write_amt > 0)
+						? wmsg->transfers.start + 1
+						: wmsg->transfers.start;
+
+		/* To avoid infinite regress, receive acknowledgement
+		 * messages do not themselves increase the message counters. */
+		uint32_t ack_msgno;
+		if (wmsg->transfers.start == wmsg->transfers.end) {
+			ack_msgno = wmsg->transfers.last_msgno;
+		} else {
+			ack_msgno = wmsg->transfers.msgnos
+						    [wmsg->transfers.start];
+		}
+
+		if (next_slot < wmsg->transfers.end) {
+			size_t nmoved = (size_t)(wmsg->transfers.end -
+						 next_slot);
+			memmove(wmsg->transfers.vecs + next_slot + 1,
+					wmsg->transfers.vecs + next_slot,
+					sizeof(*wmsg->transfers.vecs) * nmoved);
+			memmove(wmsg->transfers.msgnos + next_slot + 1,
+					wmsg->transfers.msgnos + next_slot,
+					sizeof(*wmsg->transfers.msgnos) *
+							nmoved);
+		}
+		wmsg->transfers.vecs[next_slot].iov_len =
+				sizeof(struct wmsg_ack);
+		wmsg->transfers.vecs[next_slot].iov_base = ackm;
+		wmsg->transfers.msgnos[next_slot] = ack_msgno;
+
+		wmsg->transfers.end++;
+
+		ackm->messages_received = cxs->last_received_msgno;
+		cxs->last_acked_msgno = cxs->last_received_msgno;
+	ackmsg_fail:;
+	}
+
 	int ret = partial_write_transfer(chanfd, &wmsg->transfers,
 			&wmsg->total_written, wmsg->max_iov);
 	if (ret < 0) {
@@ -721,7 +783,7 @@ static int advance_waymsg_chanwrite(struct way_msg_state *wmsg,
 		for (int i = 0; i < wmsg->ntrailing; i++) {
 			transfer_add(&wmsg->transfers,
 					wmsg->trailing[i].iov_len,
-					wmsg->trailing[i].iov_base, false);
+					wmsg->trailing[i].iov_base);
 		}
 
 		wmsg->ntrailing = 0;
@@ -767,8 +829,8 @@ static int advance_waymsg_chanwrite(struct way_msg_state *wmsg,
 	return 0;
 }
 static int advance_waymsg_progread(struct way_msg_state *wmsg,
-		struct cross_state *cxs, struct globals *g, int progfd,
-		bool display_side, bool progsock_readable)
+		struct globals *g, int progfd, bool display_side,
+		bool progsock_readable)
 {
 	const char *progdesc = display_side ? "compositor" : "application";
 	// We have data to read from programs/pipes
@@ -834,29 +896,6 @@ static int advance_waymsg_progread(struct way_msg_state *wmsg,
 	}
 
 	read_readable_pipes(&g->map);
-
-	/* Acknowledge the other side's transfers as soon as possible */
-	if (cxs->last_acked_msgno != cxs->last_received_msgno) {
-		struct wmsg_ack *ackm = calloc(1, sizeof(struct wmsg_ack));
-		if (!ackm) {
-			wp_error("Failed to allocate transfer acknowledgement message");
-			goto ackmsg_fail;
-		}
-		ackm->size_and_type = transfer_header(
-				sizeof(struct wmsg_ack), WMSG_ACK_NBLOCKS);
-
-		/* To avoid infinite regress, receive acknowledgement
-		 * messages do not themselves increase the message counters. */
-		if (transfer_add(&wmsg->transfers, sizeof(struct wmsg_ack),
-				    ackm, true) == -1) {
-			wp_error("Failed to allocate space for ack message transfer");
-			free(ackm);
-			goto ackmsg_fail;
-		}
-		ackm->messages_received = cxs->last_received_msgno;
-		cxs->last_acked_msgno = cxs->last_received_msgno;
-	ackmsg_fail:;
-	}
 
 	for (struct shadow_fd_link *lcur = g->map.link.l_next,
 				   *lnxt = lcur->l_next;
@@ -966,8 +1005,8 @@ static int advance_waymsg_transfer(struct globals *g,
 		return advance_waymsg_chanwrite(
 				wmsg, cxs, g, chanfd, display_side);
 	} else if (wmsg->state == WM_WAITING_FOR_PROGRAM) {
-		return advance_waymsg_progread(wmsg, cxs, g, progfd,
-				display_side, progsock_readable);
+		return advance_waymsg_progread(wmsg, g, progfd, display_side,
+				progsock_readable);
 	}
 	return 0;
 }

src/shadow.c
@@ -1039,7 +1039,7 @@ static void add_dmabuf_create_request(struct transfer_queue *transfers,
 	memcpy(data + sizeof(struct wmsg_open_dmabuf), &sfd->dmabuf_info,
 			sizeof(struct dmabuf_slice_data));
 
-	transfer_add(transfers, padded_len, data, false);
+	transfer_add(transfers, padded_len, data);
 }
 
 static void add_dmabuf_create_request_v2(struct transfer_queue *transfers,
@@ -1064,7 +1064,7 @@ static void add_dmabuf_create_request_v2(struct transfer_queue *transfers,
 	memcpy(data + sizeof(*header), &sfd->dmabuf_info,
 			sizeof(struct dmabuf_slice_data));
 
-	transfer_add(transfers, actual_len, data, false);
+	transfer_add(transfers, actual_len, data);
 }
 static void add_file_create_request(
 		struct transfer_queue *transfers, struct shadow_fd *sfd)
@@ -1076,7 +1076,7 @@ static void add_file_create_request(
 	header->size_and_type = transfer_header(
 			sizeof(struct wmsg_open_file), WMSG_OPEN_FILE);
 
-	transfer_add(transfers, sizeof(struct wmsg_open_file), header, false);
+	transfer_add(transfers, sizeof(struct wmsg_open_file), header);
 }
 
 void finish_update(struct shadow_fd *sfd)
@@ -1141,7 +1141,7 @@ void collect_update(struct thread_pool *threads, struct shadow_fd *sfd,
 					WMSG_EXTEND_FILE);
 
 			transfer_add(transfers, sizeof(struct wmsg_open_file),
-					header, false);
+					header);
 
 			sfd->remote_bufsize = sfd->buffer_size;
 		}
@@ -1258,7 +1258,7 @@ void collect_update(struct thread_pool *threads, struct shadow_fd *sfd,
 			createh->remote_id = sfd->remote_id;
 
 			transfer_add(transfers, sizeof(struct wmsg_basic),
-					createh, false);
+					createh);
 		}
 
 		if (sfd->pipe.recv.used > 0) {
@@ -1274,7 +1274,7 @@ void collect_update(struct thread_pool *threads, struct shadow_fd *sfd,
 					(size_t)sfd->pipe.recv.used);
 			memset(buf + msgsz, 0, alignz(msgsz, 4) - msgsz);
 
-			transfer_add(transfers, alignz(msgsz, 4), buf, false);
+			transfer_add(transfers, alignz(msgsz, 4), buf);
 
 			sfd->pipe.recv.used = 0;
 		}
@@ -1287,7 +1287,7 @@ void collect_update(struct thread_pool *threads, struct shadow_fd *sfd,
 					WMSG_PIPE_SHUTDOWN_W);
 			header->remote_id = sfd->remote_id;
 			transfer_add(transfers, sizeof(struct wmsg_basic),
-					header, false);
+					header);
 			sfd->pipe.remote_can_write = false;
 		}
 		if (!sfd->pipe.can_write && sfd->pipe.remote_can_read) {
@@ -1298,7 +1298,7 @@ void collect_update(struct thread_pool *threads, struct shadow_fd *sfd,
 					WMSG_PIPE_SHUTDOWN_R);
 			header->remote_id = sfd->remote_id;
 			transfer_add(transfers, sizeof(struct wmsg_basic),
-					header, false);
+					header);
 			sfd->pipe.remote_can_read = false;
 		}
 	} break;

src/util.c
@@ -385,33 +385,37 @@ const char *wmsg_type_to_str(enum wmsg_type tp)
 	return wmsg_types[tp];
 }
 
-int transfer_add(struct transfer_queue *w, size_t size, void *data,
-		bool is_ack_msg)
+int transfer_ensure_size(struct transfer_queue *transfers, int count)
+{
+	int sz = transfers->size;
+	if (buf_ensure_size(count, sizeof(*transfers->vecs), &sz,
+			    (void **)&transfers->vecs) == -1) {
+		return -1;
+	}
+	sz = transfers->size;
+	if (buf_ensure_size(count, sizeof(*transfers->msgnos), &sz,
+			    (void **)&transfers->msgnos) == -1) {
+		return -1;
+	}
+	transfers->size = sz;
+	return 0;
+}
+
+int transfer_add(struct transfer_queue *w, size_t size, void *data)
 {
 	if (size == 0) {
 		return 0;
 	}
-	int sz = w->size;
-	if (buf_ensure_size(w->end + 1, sizeof(struct iovec), &sz,
-			    (void **)&w->vecs) == -1) {
+	if (transfer_ensure_size(w, w->end + 1) == -1) {
 		return -1;
 	}
-	sz = w->size;
-	if (buf_ensure_size(w->end + 1, sizeof(struct iovec), &sz,
-			    (void **)&w->msgnos) == -1) {
-		return -1;
-	}
-	w->size = sz;
 
 	w->vecs[w->end].iov_len = size;
 	w->vecs[w->end].iov_base = data;
 	w->msgnos[w->end] = w->last_msgno;
 	w->end++;
-	if (!is_ack_msg) {
-		w->last_msgno++;
-	}
-
-	return true;
+	w->last_msgno++;
+	return 0;
 }
 
 void transfer_async_add(struct thread_msg_recv_buf *q, void *data, size_t sz)
@@ -441,7 +445,7 @@ int transfer_load_async(struct transfer_queue *w)
 		}
 		/* Only fill/diff messages are received async, so msgno
 		 * is always incremented */
-		if (transfer_add(w, v.iov_len, v.iov_base, false) == -1) {
+		if (transfer_add(w, v.iov_len, v.iov_base) == -1) {
 			wp_error("Failed to add message to transfer queue");
 			pthread_mutex_unlock(&w->async_recv_queue.lock);
 			return -1;

src/util.h
@@ -373,9 +373,12 @@ struct transfer_queue {
 	struct thread_msg_recv_buf async_recv_queue;
 };
 
-/** Add transfer message to the queue, expanding the queue as necessary. */
-int transfer_add(struct transfer_queue *transfers, size_t size, void *data,
-		bool is_ack_msg);
+/** Ensure the queue has space for 'count' elements */
+int transfer_ensure_size(struct transfer_queue *transfers, int count);
+/** Add transfer message to the queue, expanding the queue as necessary.
+ * This increments the last_msgno, and thus should not be used
+ * for WMSG_ACK_NBLOCKS messages. */
+int transfer_add(struct transfer_queue *transfers, size_t size, void *data);
 /** Destroy the transfer queue, deallocating all attached buffers */
 void cleanup_transfer_queue(struct transfer_queue *transfers);
 /** Move any asynchronously loaded messages to the queue */

src/video.c
@@ -1090,7 +1090,7 @@ void collect_video_from_mirror(
 		memcpy(buf + sizeof(struct wmsg_basic), pkt->buf->data, pktsz);
 		memset(buf + msgsz, 0, alignz(msgsz, 4) - msgsz);
 
-		transfer_add(transfers, alignz(msgsz, 4), buf, false);
+		transfer_add(transfers, alignz(msgsz, 4), buf);
 
 		av_packet_unref(pkt);
 	}

test/common.c
@@ -167,7 +167,7 @@ void send_wayland_msg(struct test_state *src, const struct msg msg,
 		((uint32_t *)tmsg)[0] = transfer_header(tsz, WMSG_INJECT_RIDS);
 		memcpy((char *)tmsg + 4, fd_window.data,
 				4 * (size_t)fd_window.zone_start);
-		transfer_add(transfers, tsz, tmsg, false);
+		transfer_add(transfers, tsz, tmsg);
 	}
 	if (proto_mid.zone_end > 0) {
 		size_t tsz = sizeof(uint32_t) + (size_t)proto_mid.zone_end;
@@ -175,7 +175,7 @@ void send_wayland_msg(struct test_state *src, const struct msg msg,
 		((uint32_t *)tmsg)[0] = transfer_header(tsz, WMSG_PROTOCOL);
 		memcpy((char *)tmsg + 4, proto_mid.data,
 				(size_t)proto_mid.zone_end);
-		transfer_add(transfers, tsz, tmsg, false);
+		transfer_add(transfers, tsz, tmsg);
 	}
 cleanup:
 	free(proto_src.data);

test/fuzz_hook_det.c
@@ -137,8 +137,7 @@ int main(int argc, char **argv)
 			/* Send a transfer */
 			void *msg_copy = calloc(packet_size, 4);
 			memcpy(msg_copy, &data[cursor], packet_size * 4);
-			transfer_add(&transfers, packet_size * 4, msg_copy,
-					false);
+			transfer_add(&transfers, packet_size * 4, msg_copy);
 			receive_wire(&ts, &transfers);
 		}
 		cleanup_transfer_queue(&transfers);

commit 9070c4c527c906cb186588ca410d92d2f7f3c7ba

Fix a memory leak
This was introduced by a0f6bfa191f55b99e4ff68dd0063aa0c0e12dcbd
incorrectly checking when to increase the value of
cxs->last_confirmed_msgno. As a result, one of the two Waypipe
processes would leak all the messages sent to the other process.

src/mainloop.c
@@ -282,7 +282,7 @@ static int interpret_chanmsg(struct chan_msg_state *cmsg,
 		wp_debug("Received WMSG_ACK_NBLOCKS: remote recvd %u",
 				ackm->messages_received);
 		if (msgno_gt(ackm->messages_received,
-				    cxs->last_received_msgno)) {
+				    cxs->last_confirmed_msgno)) {
 			cxs->last_confirmed_msgno = ackm->messages_received;
 		}
 		return 0;